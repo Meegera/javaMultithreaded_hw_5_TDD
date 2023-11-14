85d37ffeead547bbb931700c07d702e6ca1a3cd3
Greta Donskova <gretadonskova@gmail.com>
2023-11-15 02:49:01 +0300

    add implemented

--- src/main/java/org/example/PhoneBook.java
+++ src/main/java/org/example/PhoneBook.java
@@ -1,7 +1,16 @@
 package org.example;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PhoneBook {
+    static Map<String, String> phoneBook = new HashMap<>();
+    static int counter = 0;
     public static int add(String name, String phoneNumber){
-        return 0;
+        if(!phoneBook.containsKey(name)){
+            phoneBook.put(name, phoneNumber);
+            counter++;
+        }
+        return counter;
     }
 }
